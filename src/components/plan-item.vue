408b3255bb715fac1d5029767c6e2895e18ddd8d
LeoDanSG <sleod239@hotmail.com>
2024-04-16 12:06:37 -0600

    🌠FEAT: Properties (Checkpoint)

--- src/components/plan-item.vue
+++ src/components/plan-item.vue
@@ -2,7 +2,7 @@
     <div class="plan">
         <div class="description">
             <span class="title">
-                {{ name }} 👍 Likes: {{ likes }}
+                {{ name }}
             </span>
         </div>
       </div>
@@ -13,11 +13,7 @@
 defineProps({
     name: {
         type: String,
-        required: true,
-    },
-    likes: {
-        type: Number,
-        default: 0,
+        required: true
     }
 });
 </script>
