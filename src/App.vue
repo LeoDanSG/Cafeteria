cb2fce34dfa407b32628dbc42284f0b56ca6c405
LeoDanSG <sleod239@hotmail.com>
2024-04-18 11:43:19 -0600

    🌟Nested Components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <script setup>
-import { ref } from 'vue';
-import planItem from "./components/plan-item.vue";
-const plans = ref([
-  "El soltero",
-  "El adicto",
-  "El viajero",
-  "El colombiano",
-  "El italiano"]);
+import planPicker from './components/plan-picker.vue';
 </script>
 
 <template>
@@ -23,13 +16,9 @@ const plans = ref([
     <h2 class="subtitle">
       Viajamos por el mundo para encontrar el mejor café de origen único para ti
     </h2>
-
-    <div class="plans">
-      <plan-item 
-      v-for="plan in plans" 
-      :name="plan"
-      v-bind:key="plan" />
-    </div>
+   
+    <plan-picker />
+  
   </div>
 </template>
 
