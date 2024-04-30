17f7a1be84687afb1f943329538ce750f3edbc58
LeoDanSG <sleod239@hotmail.com>
2024-04-30 13:01:28 -0600

    S05A01 Fancy Button

---
+++ src/components/FancyButton.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+    <div class="content">
+        <button>Haz Clic 😘</button>
+    </div>
+</template>
+
+<style scoped>
+.content{
+  text-align: center;
+}
+
+button{
+  width: 12rem;
+  margin: 30px auto;
+  font-size: 2rem;
+}
+</style>
