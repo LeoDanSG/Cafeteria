f84c2d001d5343903285b7cba8cf5c5ea0890ad5
LeoDanSG <sleod239@hotmail.com>
2024-04-30 12:34:13 -0600

    🎉FIN Coffee-Plans

--- src/components/plan-picker.vue
+++ src/components/plan-picker.vue
@@ -1,16 +1,18 @@
 <template>
-    <div @click="select" class="plans">
+    <div ref="plansWrapper"  
+    @click="select" class="plans">
       <plan-picker-item
       @select="printSelected"
       :selectedPlan="selectedPlan"
       v-for="plan in plans" 
       :name="plan"
       v-bind:key="plan" />
+      <p style="font-size: 25px;">Counter: {{ counter }}</p>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 import planPickerItem from './plan-picker-item.vue';
 const plans = ref([
   "El soltero",
@@ -19,9 +21,34 @@ const plans = ref([
   "El colombiano",
   "El italiano"]);
 
+  const plansWrapper = ref (null);
+
   const selectedPlan = ref(null);
 
   const printSelected = (playload) => {
     selectedPlan.value = playload;
   }
+  
+  //Creando una referencia reactiva para un contador
+  const counter = ref(0);
+  //Creando un metodo para incrementar el contador
+  const processId = setInterval(() => {
+    console.log('Incrementando contador');
+    counter.value++;
+  }, 1000);
+
+
+  //Registrando el CB (Call Back) onMounted
+  onMounted(() => {
+      //Obteniendo la referencia del elemento .plans
+      console.log('Componente Plan Picker Montado');
+  console.log(plansWrapper.value);
+  });
+
+  //Registrando el CB (Call Back) onUnmounted
+  onUnmounted(() => {
+      //Obteniendo la referencia del elemento .plans
+  console.log('Componente Plan Picker desmontado');
+  clearInterval(processId);
+  });
 </script>
